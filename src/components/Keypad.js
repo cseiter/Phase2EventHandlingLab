729b7827bfd5f6ea8c139d29c33a581bc1592d84
chris <cseiter@gmail.com>
2021-05-16 20:36:08 -0400

    Keypad component works, commented out EyesOnMe

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+
+import React from 'react';
+
+function Keypad() {
+  function enterPassword() {
+    console.log("Entering password...")
+  }
+  return (
+    <input type="password" onChange={enterPassword} />
+  )
+}
+export default Keypad;
